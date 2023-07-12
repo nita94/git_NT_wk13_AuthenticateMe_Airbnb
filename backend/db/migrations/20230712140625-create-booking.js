f6b06116061d2d0b59ba7fd2e555a3cc7fe9ae02
nickt <nicktan17@gmail.com>
2023-07-12 09:38:58 -0500

    updated and completed migrations

--- backend/db/migrations/20230712140625-create-booking.js
+++ backend/db/migrations/20230712140625-create-booking.js
@@ -1,22 +1,65 @@
 'use strict';
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Bookings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      spotId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Spots' },
+        onDelete: 'CASCADE',
+        hooks: true
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Users' },
+        onDelete: 'CASCADE',
+        hooks: true
+      },
+      startDate: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      endDate: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    }, options);
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    options.tableName = 'Bookings';
+    await queryInterface.addConstraint(options, {
+      fields: ['endDate'],
+      type: 'check',
+      where: {
+        endDate: { [Sequelize.Op.gte]: Sequelize.col('startDate') },
+      },
+      name: 'Bookings_endDate_check'
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Bookings';
+    await queryInterface.removeConstraint(options, 'Bookings_endDate_check');
+    await queryInterface.dropTable('Bookings');
   }
 };
