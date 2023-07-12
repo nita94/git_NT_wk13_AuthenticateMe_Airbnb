635e52a0e36256cb239ef5a43bbc721d5664f6c1
nickt <nicktan17@gmail.com>
2023-07-12 06:53:13 -0500

    Add models-reviews.js finally

---
+++ backend/db/models/reviews.js
@@ -0,0 +1,50 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    static associate(models) {
+      Review.belongsTo(models.User, { foreignKey: 'userId' });
+      Review.belongsTo(models.Spot, { foreignKey: 'spotId' });
+      Review.hasMany(models.ReviewImage, { foreignKey: 'reviewId', onDelete: 'CASCADE', hooks: true });
+    }
+  }
+
+  Review.init(
+    {
+      spotId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Spots',
+        },
+        onDelete: 'CASCADE',
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Users',
+        },
+        onDelete: 'CASCADE',
+        allowNull: false,
+      },
+      review: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      stars: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 0,
+          max: 5,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Review',
+    }
+  );
+
+  return Review;
+};
