d6424c82ae5f539f2e4b04b0c8d243b56d8757aa
nickt <nicktan17@gmail.com>
2023-07-17 02:37:49 -0500

    updated index.js under routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,25 +1,18 @@
 // backend/routes/index.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const apiRouter = require('./api');
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+const apiRouter = require("./api");
 
-// backend/routes/index.js
-// ...
+router.use("/api", apiRouter);
+
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie("XSRF-TOKEN", csrfToken);
   res.status(200).json({
-    'XSRF-Token': csrfToken
+    "XSRF-Token": csrfToken,
   });
 });
-// ...
-router.use('/api', apiRouter);
-
 
 module.exports = router;
