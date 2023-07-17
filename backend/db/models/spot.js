031530e41b67124ddfdd82f5f49bae29ff118648
nickt <nicktan17@gmail.com>
2023-07-16 22:50:06 -0500

    updated spot.js model

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -1,6 +1,7 @@
 'use strict';
-const { Model } = require('sequelize');
-
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Spot extends Model {
     /**
@@ -10,33 +11,65 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Spot.belongsTo(models.User, { foreignKey: 'ownerId' })
-      Spot.hasMany(models.SpotImage, { foreignKey: 'spotId', onDelete: 'CASCADE', hooks: true })
-      Spot.hasMany(models.Booking, { foreignKey: 'spotId', onDelete: 'CASCADE', hooks: true })
-      Spot.hasMany(models.Review, { foreignKey: 'spotId', onDelete: 'CASCADE', hooks: true })
-      Spot.belongsToMany(models.User, { through: models.Review, foreignKey: "spotId", otherKey: "userId" });
-      Spot.belongsToMany(models.User, { through: models.Booking, foreignKey: "spotId", otherKey: "userId" });
+      Spot.belongsTo(models.User, {
+        foreignKey: 'ownerId',
+        hooks: true
+      });
+      Spot.hasMany(models.Booking, {
+        foreignKey: 'spotId',
+        hooks: true
+      });
+      Spot.hasMany(models.SpotImage, {
+        foreignKey: 'spotId',
+        hooks: true
+      });
+      Spot.hasMany(models.Review, {
+        foreignKey: 'spotId',
+        hooks: true
+      })
     }
   }
-
-  Spot.init(
-    {
-      ownerId: { type: DataTypes.INTEGER, references: { model: 'Users' }, onDelete: 'CASCADE' },
-      address: { type: DataTypes.STRING, allowNull: false },
-      city: { type: DataTypes.STRING, allowNull: false },
-      state: { type: DataTypes.STRING, allowNull: false },
-      country: { type: DataTypes.STRING, allowNull: false },
-      lat: DataTypes.DECIMAL,
-      lng: DataTypes.DECIMAL,
-      name: { type: DataTypes.STRING, allowNull: false },
-      description: { type: DataTypes.STRING, allowNull: false },
-      price: { type: DataTypes.DECIMAL, allowNull: false },
+  Spot.init({
+    ownerId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Users'
+      },
+      onDelete: 'CASCADE'
     },
-    {
-      sequelize,
-      modelName: 'Spot',
-    }
-  );
-
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lat: DataTypes.DECIMAL,
+    lng: DataTypes.DECIMAL,
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+  }, {
+    sequelize,
+    modelName: 'Spot',
+  });
   return Spot;
 };
